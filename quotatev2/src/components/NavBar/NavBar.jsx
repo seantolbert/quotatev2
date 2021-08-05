c815f0bdfc730666302345344059f9596eccd588
seantolbert <84528717+seantolbert@users.noreply.github.com>
2021-08-05 16:39:50 -0600

    ok, so i was not done stying

--- quotatev2/src/components/NavBar/NavBar.jsx
+++ quotatev2/src/components/NavBar/NavBar.jsx
@@ -1,19 +1,30 @@
+import { className } from "postcss-selector-parser";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import * as userService from "../../utilities/users-service";
 import "./NavBar.css";
 
 export default function NavBar({ user, setUser }) {
+  const [isActive, setIsActive] = useState(false);
+
   function handleLogOut() {
     userService.logOut();
     setUser(null);
   }
   return (
     <div className="block">
-      <nav className="navbar is-black">
-        <div className="navbar-item">
-          <div className="title">
-            <b className="quotate">Quotate</b>
+      <nav
+        className="navbar is-black"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <div className="navbar-item">
+            <div className="title">
+              <b className="quotate">Quotate</b>
+            </div>
           </div>
+          <div></div>
         </div>
         <div className="navbar-menu">
           <div className="navbar-end">
